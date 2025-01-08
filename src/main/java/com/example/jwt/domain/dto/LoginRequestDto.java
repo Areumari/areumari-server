89317c02b679de328b239e29bcdf37f201bed97d
valhub08 <jhstl1203@gmail.com>
2025-01-08 19:44:03 +0900

    feat :: LoginRequestDto 추가

---
+++ src/main/java/com/example/jwt/domain/dto/LoginRequestDto.java
@@ -0,0 +1,9 @@
+package com.example.jwt.domain.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class LoginRequestDto extends BaseMemberRequestDto {
+}
