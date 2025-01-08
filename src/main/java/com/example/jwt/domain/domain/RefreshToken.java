6187d6d82f7c603e4d92f86710291bc060eee3dd
valhub08 <jhstl1203@gmail.com>
2025-01-08 19:44:49 +0900

    feat :: RefreshTokenEntity 추가

---
+++ src/main/java/com/example/jwt/domain/domain/RefreshToken.java
@@ -0,0 +1,26 @@
+package com.example.jwt.domain.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class RefreshToken {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String tokenValue;
+
+    public void updateValue(String tokenValue) {
+        this.tokenValue = tokenValue;
+    }
+}
