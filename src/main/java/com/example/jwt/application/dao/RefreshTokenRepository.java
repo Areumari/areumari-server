9a7dad31eab7eef18d44a00b1877550aa92bb34d
valhub08 <jhstl1203@gmail.com>
2025-01-08 19:44:56 +0900

    feat :: RefreshTokenRepository 추가

---
+++ src/main/java/com/example/jwt/application/dao/RefreshTokenRepository.java
@@ -0,0 +1,11 @@
+package com.example.jwt.application.dao;
+
+import com.example.jwt.domain.domain.RefreshToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+    Optional<RefreshToken> findByEmail(String email);
+    Optional<RefreshToken> findByTokenValue(String tokenValue);
+}
