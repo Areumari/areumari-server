f83c0451e9497f5f7e7f564d8fc626fbf2e8ade9
valhub08 <jhstl1203@gmail.com>
2025-01-08 19:43:27 +0900

    feat :: CustomUserDetailsService 추가

---
+++ src/main/java/com/example/jwt/domain/service/CustomUserDetailsService.java
@@ -0,0 +1,39 @@
+package com.example.jwt.domain.service;
+
+import com.example.jwt.application.dao.MemberRepository;
+import com.example.jwt.domain.domain.Member;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import java.util.Collections;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final MemberRepository memberRepository;
+
+    @Override
+    @Transactional(readOnly = true)
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return memberRepository.findByEmail(email)
+                .map(this::createUserDetails)
+                .orElseThrow(() -> new UsernameNotFoundException(email + " -> 해당하는 유저를 찾을 수 없습니다."));
+    }
+
+    private UserDetails createUserDetails(Member member) {
+        GrantedAuthority grantedAuthority = new SimpleGrantedAuthority(member.getAuthority().toString());
+
+        return new User(
+                member.getEmail(),
+                member.getPassword(),
+                Collections.singleton(grantedAuthority)
+        );
+    }
+}
