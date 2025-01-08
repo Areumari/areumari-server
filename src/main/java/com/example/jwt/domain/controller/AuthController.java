c746753fa6243b1b210f23064a0725fcadc2f1a6
valhub08 <jhstl1203@gmail.com>
2025-01-08 20:49:23 +0900

    feat :: swagger 코드 추가

--- src/main/java/com/example/jwt/domain/controller/AuthController.java
+++ src/main/java/com/example/jwt/domain/controller/AuthController.java
@@ -2,6 +2,8 @@ package com.example.jwt.domain.controller;
 
 import com.example.jwt.domain.dto.*;
 import com.example.jwt.domain.service.AuthService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -10,16 +12,19 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/auth")
 @RequiredArgsConstructor
+@Tag(name = "Authentication", description = "회원 인증 API") // Swagger Tag 설정
 public class AuthController {
 
     private final AuthService authService;
 
+    @Operation(summary = "회원가입", description = "회원가입을 위한 API입니다.")
     @PostMapping("/signUp")
     public ResponseEntity<String> signUp(@Valid @RequestBody SignupRequestDto requestDto) {
         authService.signup(requestDto);
         return ResponseEntity.ok("회원가입이 완료되었습니다.");
     }
 
+    @Operation(summary = "로그인", description = "로그인을 위한 API입니다. JWT 토큰과 환영 메시지를 반환합니다.")
     @PostMapping("/signIn")
     public ResponseEntity<LoginResponseDto> signIn(@Valid @RequestBody LoginRequestDto requestDto) {
         TokenDto jwtResponseDto = authService.login(requestDto);
@@ -28,6 +33,7 @@ public class AuthController {
         return ResponseEntity.ok(responseDto);
     }
 
+    @Operation(summary = "토큰 재발급", description = "Access 토큰을 재발급 받기 위한 API입니다.")
     @PostMapping("/reissue")
     public ResponseEntity<TokenDto> reissue(@RequestBody TokenRequestDto tokenRequestDto) {
         return ResponseEntity.ok(authService.reissue(tokenRequestDto));
