fcec63fb10bcc032a380ca5be6b0979d1b83a6f5
valhub08 <jhstl1203@gmail.com>
2025-02-08 16:29:51 +0900

    feat :: email 관련 request값 삭제

--- src/main/java/com/example/jwt/domain/dto/BaseMemberRequestDto.java
+++ src/main/java/com/example/jwt/domain/dto/BaseMemberRequestDto.java
@@ -1,6 +1,5 @@
 package com.example.jwt.domain.dto;
 
-import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
@@ -11,17 +10,12 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 public abstract class BaseMemberRequestDto {
-
-    @Size(min = 1, max = 10,message = "이름은 10자리 이상입력 불가합니다")
+    @Size(min = 1, max = 10, message = "이름은 10자리 이상 입력 불가합니다")
     @NotBlank(message = "이름은 필수 입력값입니다.")
     private String name;
 
-    @Size(max = 5, message = "학번은 4자리 이상입력 불가합니다")
-    private String s_number;
-
-    @Email(message = "유효한 이메일 형식을 입력하세요.")
-    @NotBlank(message = "이메일은 필수 입력값입니다.")
-    private String email;
+    @Size(max = 5, message = "학번은 5자리 초과 입력 불가합니다")
+    private String snumber;
 
     @NotBlank(message = "비밀번호는 필수 입력값입니다.")
     private String password;
