9e09151e9fa3ae96455cdc899af4a763262f4331
valhub08 <jhstl1203@gmail.com>
2025-02-08 16:30:49 +0900

    Refactor: UserDetailsService 사용자 식별 기준을 학번으로 변경

--- src/main/java/com/example/jwt/domain/service/CustomUserDetailsService.java
+++ src/main/java/com/example/jwt/domain/service/CustomUserDetailsService.java
@@ -11,27 +11,27 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
 import java.util.Collections;
 
 @Service
 @RequiredArgsConstructor
 public class CustomUserDetailsService implements UserDetailsService {
-
     private final MemberRepository memberRepository;
 
     @Override
     @Transactional(readOnly = true)
-    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        return memberRepository.findByEmail(email)
+    public UserDetails loadUserByUsername(String snumber) throws UsernameNotFoundException {
+        return memberRepository.findBySnumber(snumber)
                 .map(this::createUserDetails)
-                .orElseThrow(() -> new UsernameNotFoundException(email + " -> 해당하는 유저를 찾을 수 없습니다."));
+                .orElseThrow(() -> new UsernameNotFoundException(snumber + " -> 해당하는 유저를 찾을 수 없습니다."));
     }
 
     private UserDetails createUserDetails(Member member) {
         GrantedAuthority grantedAuthority = new SimpleGrantedAuthority(member.getAuthority().toString());
 
         return new User(
-                member.getEmail(),
+                member.getSnumber(),
                 member.getPassword(),
                 Collections.singleton(grantedAuthority)
         );
