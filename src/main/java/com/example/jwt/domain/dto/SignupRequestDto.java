d7ae5f547800b14d1e8a7a056526bce1fa210a75
valhub08 <jhstl1203@gmail.com>
2025-02-10 21:54:58 +0900

    Refactor :: 코드 가독성 증대

--- src/main/java/com/example/jwt/domain/dto/SignupRequestDto.java
+++ src/main/java/com/example/jwt/domain/dto/SignupRequestDto.java
@@ -5,11 +5,9 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-
 @Getter
 @Setter
 @NoArgsConstructor
 public class SignupRequestDto extends BaseMemberRequestDto {
-
     private Authority authority;
 }
