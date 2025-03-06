ef3a5f65a6fd032e392bef21efd0cae5d7a543e9
valhub08 <jhstl1203@gmail.com>
2025-03-06 12:42:51 +0900

    feat :: feed api 권한 설정

--- src/main/java/com/example/jwt/domain/jwt/config/SecurityConfig.java
+++ src/main/java/com/example/jwt/domain/jwt/config/SecurityConfig.java
@@ -35,6 +35,7 @@ public class SecurityConfig {
                             .requestMatchers("/api/auth/**").permitAll()
                             .requestMatchers("/swagger-ui/**").permitAll()
                             .requestMatchers("/v3/api-docs/**").permitAll()
+                            .requestMatchers("/api/feeds/**").permitAll()
                             .requestMatchers("/api/**").permitAll()
                             .anyRequest().authenticated();
                 })
