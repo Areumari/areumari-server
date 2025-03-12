6494000a686caf65dc062c01b2a71ceda19480d0
valhub08 <jhstl1203@gmail.com>
2025-03-12 18:09:07 +0900

    feat :: PostRepository

--- src/main/java/com/example/jwt/application/dao/PostRepository.java
+++ src/main/java/com/example/jwt/application/dao/PostRepository.java
@@ -4,15 +4,17 @@ import com.example.jwt.search.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
 import java.util.List;
-import java.util.Optional;
 
+@Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
-    Optional<List<Post>> findByTitleContainingIgnoreCaseOrderByCreatedAtDesc(String title);
-    Optional<List<Post>> findByCategoryContainingIgnoreCaseOrderByCreatedAtDesc(String category);
 
-    @Query("SELECT p FROM Post p WHERE LOWER(p.title) LIKE LOWER(CONCAT('%', :keyword, '%')) " +
-            "OR LOWER(p.category) LIKE LOWER(CONCAT('%', :keyword, '%')) " +
-            "ORDER BY p.createdAt DESC")
-    Optional<List<Post>> searchByTitleOrCategory(@Param("keyword") String keyword);
+    @Query(value = """
+        SELECT * FROM posts 
+        WHERE MATCH(title, category) AGAINST (:keyword IN BOOLEAN MODE)
+        ORDER BY created_at DESC
+        """, nativeQuery = true)
+    List<Post> searchByTitleOrCategory(@Param("keyword") String keyword);
 }
