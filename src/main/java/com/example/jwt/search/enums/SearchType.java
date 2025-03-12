2182d6a3d899387f789767be3abc8d2d6353c84f
valhub08 <jhstl1203@gmail.com>
2025-03-12 18:09:35 +0900

    feat :: SearchType

--- src/main/java/com/example/jwt/search/enums/SearchType.java
+++ src/main/java/com/example/jwt/search/enums/SearchType.java
@@ -1,5 +1,9 @@
 package com.example.jwt.search.enums;
 
 public enum SearchType {
-    TITLE, CATEGORY, BOTH
+    TITLE,
+    CATEGORY,
+    BOTH,
+    FULLTEXT,  // FullText 검색 타입
+    LIKE;      // LIKE 검색 타입
 }
