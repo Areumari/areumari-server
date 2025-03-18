a35115689df7443bbe6ff73d5730a6774cc8b93a
valhub08 <jhstl1203@gmail.com>
2025-03-18 10:26:34 +0900

    feat :: after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
+cd /home/ubuntu/areumari-server || exit
 
-ROOT_PATH="/home/ubuntu/areumari-server"
-INSTALL_LOG="$ROOT_PATH/install.log"
+# 환경 변수 파일 생성 (RDS 접속 정보 등)
+cat > .env << EOL
+DB_HOST=database-2.c186ei0q4wh2.ap-northeast-2.rds.amazonaws.com
+DB_USER=admin
+DB_PASSWORD=amgfaa289
+DB_NAME=database-2
+SPRING_PROFILES_ACTIVE=prod
+EOL
 
-NOW=$(date +%c)
+# 스크립트 실행 권한 부여
+chmod +x ./scripts/*.sh
 
-echo "[$NOW] 배포 파일 권한 설정" >> $INSTALL_LOG
-chmod +x $ROOT_PATH/*.jar
-chmod +x $ROOT_PATH/scripts/*.sh
+# Docker 이미지 빌드
+sudo docker build -t areumari-server .
+
+exit 0
